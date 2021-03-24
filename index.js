6d3d67bf7dc34f79b44ee50858dae146cf7f975e
Jon Narine <jonnicwolf@gmail.com>
2021-03-24 03:36:42 -0400

    array[i].image is undefined?

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
 // const axios = require('axios');
 const makeLi = () => {
-    console.log('hey!')
     const li = document.createElement('li');
     const ul = document.querySelector('#all-characters')
     li.innerHTML = '<img class="photo-display"><p></p>'
-    ul.appendChild(li)
-    console.log(ul)
+    ul.appendChild(li)    
 }
 
 const npcGrab = () => {
     axios.get('https://rickandmortyapi.com/api/character')
         .then(characters => {
             const array = characters.data.results
-            console.log(array)
-        
+
             //for each index, an li with appropriate contents then append them to the ul
             // array.forEach(makeLi)
+            // array.forEach((indx) => {
+            //     const img = document.querySelector('.photo-display')
+            //     const para = document.getElementsByTagName('p')
+            
+            //     img.src = array[i].image[i]
+            //     console.log(img)
+            //     para.textContent = array.name
+            // })
 
-            for (let i = 0; i < array.length; i++){
-                console.log(array[i])
-                array[i]
+            for (let i = 0; i <= array.length; i++) {
+                // console.log(array[i].name)
+                
+            
                 makeLi()
-            }
-
-
-
-            const character = array.name
-            const characterImg = array.image
-            //grab the new tags and give them content
-            array.forEach(() => {
-                const img = document.QuerySelector('.photo-display')
+            
+                const img = document.querySelector('.photo-display')
                 const para = document.getElementsByTagName('p')
-                img.src = characterImg
-                para.textContent = character
-                console.log(img)
-            })
-            //give the new tags their content
-            // const characterArr = characters.results
-            // charactArr.forEach(makeLi)
-            // console.log(characterArr)
-            // img.src = characterArr.id
-            // console.log(Object.keys(characters))
-            // para.textContent = characterArr.name
-            // console.log(characterArr.name)
+                const characterImg = array[i].image.toStringify
+                // const character = array[i].name
+                // console.log(characters)
+                console.log(characterImg)
+                // console.log(character, characterImg)
+                // img.src = characterImg
+                // para.textContent = character
+                // console.log(para, img)
+                // img.src = array[i].image
+                // para.textContent = array[i].name
+                // console.log(para)
+                // img.src = characterImg
+                // console.log(img)
+                // para.textContent = character
+                // console.log(img)
+
+            }
         })
-        .catch(err => console.log(err))
+        
+    
+    .catch(err => console.log(err))
 }
 npcGrab()
